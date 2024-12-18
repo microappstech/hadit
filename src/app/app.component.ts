c58d51fec880e943880f9167f907f20efa01b479
microappstech <hamzamouddakur@gmail.com>
2024-12-18 23:30:55 +0100

    day 1 : 18_12_2024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { FooterComponent } from "../components/footer/footer.component";
+import { NavbarComponent } from "../components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  showNavbar = true;
+  showFooter = true;
   title = 'hadit';
+  constructor(private router:Router)
+  {
+   this.router.events.subscribe(()=>{
+    const currentRoute = this.router.url;
+    // console.log(currentRoute)
+    // this.showNavbar = currentRoute != "/login";
+    // this.showFooter = currentRoute != "/login";
+   }); 
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from '../Pages/home/home.component';
+import { ContactComponent } from '../Pages/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path : "", component:HomeComponent},
+    { path:"idea", component:ContactComponent}
+];
