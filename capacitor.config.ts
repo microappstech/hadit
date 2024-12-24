175fbe575da22c53cec30b75d5e2879b7b12dcea
microappstech <hamzamouddakur@gmail.com>
2024-12-24 20:04:24 +0100

    add apps

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'hadit',
+  webDir: 'dist/haddit'
+};
+
+export default config;
